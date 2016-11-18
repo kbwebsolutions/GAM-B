d5ee2d181e2e63b5d1e2579c94856d598b8c23b6
Ross Scroggs <ross.scroggs@gmail.com>
2016-11-18 08:20:45 -0800

    Improvements to handling Google misreporting invalid group in gam print groups settings.

--- src/gam.py
+++ src/gam.py
@@ -23,7 +23,7 @@ For more information, see https://github.com/taers232c/GAM-B
 """
 
 __author__ = u'Ross Scroggs <ross.scroggs@gmail.com>'
-__version__ = u'4.03.08'
+__version__ = u'4.03.09'
 __license__ = u'Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)'
 
 import sys
@@ -9253,11 +9253,11 @@ def doPrintGroups():
         group[u'Owners'] = memberDelimiter.join(allOwners)
     if getSettings and not GroupIsAbuseOrPostmaster(groupEmail):
       sys.stderr.write(u" Retrieving Settings for group %s (%s/%s)...\r\n" % (groupEmail, i, count))
-      try:
-        settings = callGAPI(gs.groups(), u'get',
-                            retry_reasons=[u'serviceLimit'],
-                            throw_reasons=[u'invalid'],
-                            groupUniqueId=groupEmail, fields=gsfields)
+      settings = callGAPI(gs.groups(), u'get',
+                          soft_errors=True,
+                          retry_reasons=[u'serviceLimit', u'invalid'],
+                          groupUniqueId=groupEmail, fields=gsfields)
+      if settings:
         for key in settings:
           if key in [u'email', u'name', u'description', u'kind', u'etag']:
             continue
@@ -9267,7 +9267,7 @@ def doPrintGroups():
           if key not in titles:
             addTitleToCSVfile(key, titles)
           group[key] = setting_value
-      except googleapiclient.errors.HttpError:
+      else:
         sys.stderr.write(u" Settings unavailable for group %s (%s/%s)...\r\n" % (groupEmail, i, count))
     csvRows.append(group)
   writeCSVfile(csvRows, titles, u'Groups', todrive)
