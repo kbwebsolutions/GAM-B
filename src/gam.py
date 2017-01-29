e1d6cabd1c3f0f029293879a8a04c65aa21ee0ca
Ross Scroggs <ross.scroggs@gmail.com>
2017-01-29 13:18:29 -0800

    Bump version

--- src/gam.py
+++ src/gam.py
@@ -23,7 +23,7 @@ For more information, see https://github.com/taers232c/GAM-B
 """
 
 __author__ = u'Ross Scroggs <ross.scroggs@gmail.com>'
-__version__ = u'4.11.02'
+__version__ = u'4.11.03'
 __license__ = u'Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)'
 
 import sys
